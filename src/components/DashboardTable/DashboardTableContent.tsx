bb291782bb4c7a7a917cdfec26c6c7fb6d37c338
SemzyTheMan <semzy2002@gmail.com>
2024-11-06 00:06:51 +0100

    feat:suspense error fix

--- src/components/DashboardTable/DashboardTableContent.tsx
+++ src/components/DashboardTable/DashboardTableContent.tsx
@@ -5,6 +5,7 @@ import PaginateComponent from "../Pagination/PaginateComponent";
 import { columns, Dashboard } from "./columns";
 import { DataTable } from "./data-table";
 import styles from "./Table.module.scss";
+import { Suspense } from "react";
 
 function getData(): Dashboard[] {
   // Fetch data from your API here.
@@ -37,7 +38,7 @@ export default function DashboardTableContent() {
   const last = page * 100;
 
   return (
-    <>
+    <Suspense>
       <DataTable columns={columns} data={data.slice(first, last)} />
       <div className={`${styles.pagination_container}`}>
         <p className={`${styles.pagination_first}`}>
@@ -58,6 +59,6 @@ export default function DashboardTableContent() {
           onPageChange={handlePageChange}
         />
       </div>
-    </>
+    </Suspense>
   );
 }
